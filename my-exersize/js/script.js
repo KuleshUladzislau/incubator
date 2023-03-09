c0fdc296387c52e1b9092813a74ce2cd62d3e4cf
Uladzislau Kulesh <kulesh_uladzislau@mail.ru>
2023-03-09 15:03:13 +0300

    last commit in 09.03.2023

--- my-exersize/js/script.js
+++ my-exersize/js/script.js
@@ -111,3 +111,18 @@ function narcissistic(value) {
 }
 
 
+
+// Is a number prime? 6 kyu
+
+function isPrime(num) {
+    if (num < 2) return false;
+    const limit = Math.sqrt(num);
+    for (let i = 2; i <= limit; ++i) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
